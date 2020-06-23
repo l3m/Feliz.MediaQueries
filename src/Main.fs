e15b912086ab0455cc845b1553b6b73b1b31219e
Jonas Bösch <jb@vlot.ch>
2020-06-24 00:47:10 +0200

    use media query hook

--- src/Main.fs
+++ src/Main.fs
@@ -3,17 +3,8 @@ module Main
 open Fable.Core.JsInterop
 
 importAll "../styles/main.scss"
+open Feliz
 
-open Elmish
-open Elmish.React
-open Elmish.Debug
-open Elmish.HMR
 
-// App
-Program.mkProgram App.init App.update App.render
-#if DEBUG
-|> Program.withDebugger
-#endif
-|> Program.withReactSynchronous "feliz-app"
-|> Program.run
-
+open Browser.Dom
+ReactDOM.render(App.mqTest, document.getElementById "feliz-app")
--- src/UseMediaQuery.fs
+++ src/UseMediaQuery.fs
@@ -4,22 +4,6 @@ open System
 open Browser
 open Browser.Types
 open Feliz
-open Fable.Core.JsInterop
-(*import {useEffect, useState} from 'react';
-
-export const useMediaQuery = (query) => {
-  const mediaMatch = window.matchMedia(query);
-  const [matches, setMatches] = useState(mediaMatch.matches);
-
-  useEffect(() => {
-    const handler = e => setMatches(e.matches);
-    mediaMatch.addListener(handler);
-    return () => mediaMatch.removeListener(handler);
-  });
-  return matches;
-};*)
-open System.Runtime.CompilerServices
-
    
 type [<AllowNullLiteral>] MediaQueryListEvent =    
     inherit Event
